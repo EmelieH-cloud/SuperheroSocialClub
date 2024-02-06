32bfb870b5f26af7ef7b8cc0c3903d2b7248f73e
emeliehensfelt <emelie046@gmail.com>
2024-02-06 14:35:04 +0100

    props

--- Models/SuperheroModel.cs
+++ Models/SuperheroModel.cs
@@ -2,5 +2,12 @@
 {
     public class SuperheroModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Superpower { get; set; }
+
+        public string? SecretIdentity { get; set; }
     }
 }
